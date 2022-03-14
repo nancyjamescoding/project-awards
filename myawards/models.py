712e58fc6dba5d37f889099c83c723f9fbb76541
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-03-14 15:14:51 +0300

    Created projects model and made migrations

--- myawards/models.py
+++ myawards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from time import timezone
+from django.utils import timezone
 
 # Create your models here.
+
+class Project(models.Model):
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.caption
--- myawards/views.py
+++ myawards/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from . models import Project
 
 # Create your views here.
 def awards(request):
-    return render (request, 'myawards/awards.html')
+    projects=Project.objects.all()
+    context = { 'projects': projects}
+    return render (request, 'myawards/awards.html', context)
 
 def viewawards(request, pk):
-    return render (request, 'myawards/myawards.html')
+    projects=Project.objects.get(id=pk)
+    return render (request, 'myawards/myawards.html',{'projects': projects})
 
 def addawards(request):
     return render (request, 'myawards/add.html')        
