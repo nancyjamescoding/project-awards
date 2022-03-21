a54a792395de0f05607ed922281f9458b45e9823
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-03-21 04:23:24 +0300

    Installed django heroku

--- awards/settings.py
+++ awards/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 import dj_database_url
 from decouple import config,Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,16 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'awards.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
